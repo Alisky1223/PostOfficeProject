267420dec29c0202fb15beba62e5c1c88698a21f
Administrator <Administrator@AMIRREZA>
2025-10-21 15:44:50 +0330

    1."Products"  : Impelemented- Basic Edit And Add Option Is impelementing.

---
+++ Frontend/PostOfficeFrontendProject/PostOfficeFrontendProject--all-interactive/Mapper/ValueTypesMapper.cs
@@ -0,0 +1,21 @@
+﻿using CommonDll.Dto;
+namespace PostOfficeFrontendProject__all_interactive.Mapper
+{
+    public static class ValueTypesMapper
+    {
+        //correctors
+        public static string ProductsTypeToString(this ProductTypeDto productType)
+        {
+            if (productType != null)
+            {
+                return productType.Type;
+
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
+
