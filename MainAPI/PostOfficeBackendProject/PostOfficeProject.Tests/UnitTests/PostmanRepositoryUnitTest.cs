f2d4304b39e555ea3de9ca69baa7871ed4712e03
ali.r.asadi75 <ali.r.asadi75@gmail.com>
2025-11-10 10:48:18 +0330

    1."unit test": implemented - I tried to cover all scenarios of postman repository

--- MainAPI/PostOfficeBackendProject/PostOfficeProject.Tests/UnitTests/PostmanRepositoryUnitTest.cs
+++ MainAPI/PostOfficeBackendProject/PostOfficeProject.Tests/UnitTests/PostmanRepositoryUnitTest.cs
@@ -29,7 +29,7 @@ namespace PostOfficeProject.Tests.UnitTests
         }
 
         [Fact]
-        public async Task GetAllPostmenAsync_ReturnsListOfPostmen()
+        public async Task GetPostmanByIdAsync_ReturnsPostmen()
         {
             // Arrange
             var newpostman = new Postman
@@ -49,11 +49,157 @@ namespace PostOfficeProject.Tests.UnitTests
             // Assert
             Assert.NotNull(result);
             Assert.IsType<Postman>(result);
-            Assert.Equal(newpostman.Id, result.Id);
-            Assert.Equal(newpostman.PostOfficeId, result.PostOfficeId);
-            Assert.Equal(newpostman.UserId, result.UserId);
-            Assert.Equal(newpostman.PersonalCode, result.PersonalCode);
+            ComparePostmansEqual(result, newpostman);
         }
 
+        [Fact]
+        public async Task GetPostmanByIdAsync_ReturnsNull()
+        {
+            // Arrange
+            // Act
+            var result = await _repository.GetPostmanById(1);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task GetPostmanByUserIdAsync_ReturnsPostmen()
+        {
+            // Arrange
+            var newpostman = new Postman
+            {
+                Id = 1,
+                PostOfficeId = 1,
+                UserId = 1,
+                PersonalCode = "EMP123"
+            };
+
+            await _dbContext.Postman.AddAsync(newpostman);
+            await _dbContext.SaveChangesAsync();
+
+            // Act
+            var result = await _repository.GetPostmanByUserId(1);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<Postman>(result);
+            ComparePostmansEqual(result, newpostman);
+        }
+
+
+        [Fact]
+        public async Task GetPostmanByUserIdAsync_ReturnsNull()
+        {
+            // Arrange
+
+            // Act
+            var result = await _repository.GetPostmanByUserId(1);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task GetAllPostmen() 
+        {
+            // Arrange
+            var firstPostman = new Postman
+            {
+                Id = 1,
+                PostOfficeId = 1,
+                UserId = 1,
+                PersonalCode = "EMP123"
+            };
+
+            await _dbContext.Postman.AddAsync(firstPostman);
+
+            var secondPostman = new Postman
+            {
+                Id = 2,
+                PostOfficeId = 2,
+                UserId = 2,
+                PersonalCode = "EMP456"
+            };
+
+            await _dbContext.Postman.AddAsync(secondPostman);
+
+            await _dbContext.SaveChangesAsync();
+
+            // Act
+            var result = await _repository.GetPostmen();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<Postman>>(result);
+            ComparePostmansEqual(firstPostman, result[0]);
+            ComparePostmansEqual(secondPostman, result[1]);
+        }
+
+        [Fact]
+        public async Task PostmanRepository_CreatePostman_ShouldCreateNewPostman() 
+        {
+            // Arrange
+            var newpostman = new Postman
+            {
+                Id = 1,
+                PostOfficeId = 1,
+                UserId = 1,
+                PersonalCode = "EMP123"
+            };
+
+            // Act
+            var result = await _repository.Create(newpostman);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<Postman>(result);
+            ComparePostmansEqual(result, newpostman);
+        }
+
+        [Fact]
+        public async Task PostmanRepository_CreatePostman_ShouldReturnAllreadyPostman()
+        {
+            // Arrange
+            var postman = new Postman
+            {
+                Id = 1,
+                PostOfficeId = 1,
+                UserId = 1,
+                PersonalCode = "EMP123"
+            };
+            await _repository.Create(postman);
+
+            var newpostman = new Postman
+            {
+                Id = 2,
+                PostOfficeId = 2,
+                UserId = 1,
+                PersonalCode = "EMP456"
+            };
+            // Act
+            var result = await _repository.Create(newpostman);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<Postman>(result);
+            ComparePostmansEqual(result, postman);
+            ComparePostmansNotEqual(result, newpostman);
+        }
+
+        private void ComparePostmansEqual(Postman result, Postman expected) 
+        {
+            Assert.Equal(expected.Id, result.Id);
+            Assert.Equal(expected.PostOfficeId, result.PostOfficeId);
+            Assert.Equal(expected.UserId, result.UserId);
+            Assert.Equal(expected.PersonalCode, result.PersonalCode);
+        }
+
+        private void ComparePostmansNotEqual(Postman result, Postman expected)
+        {
+            Assert.NotEqual(expected.Id, result.Id);
+            Assert.NotEqual(expected.PostOfficeId, result.PostOfficeId);
+            Assert.NotEqual(expected.PersonalCode, result.PersonalCode);
+        }
     }
 }
