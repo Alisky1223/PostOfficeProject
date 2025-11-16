4ddb3b3e773f0f6dbd41433acaa40c0b26eeab39
ali.r.asadi75 <ali.r.asadi75@gmail.com>
2025-11-16 09:19:46 +0330

    1."blazor .csproject": fixed - I have fixed the common.dll refrence in blazor .csproject file

--- src/App/BlazorFrontend/Middelware/LoginMiddleware.cs
+++ src/App/BlazorFrontend/Middelware/LoginMiddleware.cs
@@ -1,10 +1,5 @@
-﻿using CommonDll.Dto;
-using Microsoft.AspNetCore.Authentication;
-using PostOfficeProject.Core.src.Application.Dto;
-using PostOfficeFrontendProject__all_interactive.Helper;
+﻿using PostOfficeFrontendProject__all_interactive.Helper;
 using PostOfficeFrontendProject__all_interactive.Interface;
-using System.Net.Http.Json;
-using System.Text.Json;
 
 namespace PostOfficeFrontendProject__all_interactive.Middelware
 {
@@ -19,7 +14,7 @@ namespace PostOfficeFrontendProject__all_interactive.Middelware
         }
 
         public async Task<ApiResponse<string>> Login(LoginDto user)
-        {      
+        {
             try
             {
                 var response = await _httpClient.PostAsJsonAsync("/api/auth/login", user);
